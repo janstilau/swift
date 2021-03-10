133fa56d23daf8c6b6b1188a7bace69edec8b503
JustinLau <janstilau@gmail.com>
2021-03-10 10:57:00 +0800

    init project

---
+++ stdlib/public/core/SwiftCore/SwiftCore/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftCore
+//
+//  Created by JustinLau on 2021/3/10.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ stdlib/public/runtime/SwiftRuntime/SwiftRuntime/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SwiftRuntime
+//
+//  Created by JustinLau on 2021/3/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
