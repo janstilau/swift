dd39b665c313a8b6d171ef9e108937f336200827
liuguoqiang <liugq01@rd.netease.com>
2021-11-20 21:35:15 +0800

    sync

--- stdlib/public/core/Algorithm.swift
+++ stdlib/public/core/Algorithm.swift
@@ -1,15 +1,3 @@
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
-
 /// Returns the lesser of two comparable values.
 ///
 /// - Parameters:
@@ -18,12 +6,18 @@
 /// - Returns: The lesser of `x` and `y`. If `x` is equal to `y`, returns `x`.
 @inlinable // protocol-only
 public func min<T: Comparable>(_ x: T, _ y: T) -> T {
-  // In case `x == y` we pick `x`.
-  // This preserves any pre-existing order in case `T` has identity,
-  // which is important for e.g. the stability of sorting algorithms.
-  // `(min(x, y), max(x, y))` should return `(x, y)` in case `x == y`.
-  return y < x ? y : x
+    // In case `x == y` we pick `x`.
+    // This preserves any pre-existing order in case `T` has identity,
+    // which is important for e.g. the stability of sorting algorithms.
+    // `(min(x, y), max(x, y))` should return `(x, y)` in case `x == y`.
+    return y < x ? y : x
 }
+/*
+    和 C++ 不同, 这里, 必须是使用 Comparable 的协议, 才能使用 min 这方法.
+    显示的进行协议的指定.
+    隐式的进行协议的指定.
+ */
+
 
 /// Returns the least argument passed.
 ///
@@ -34,14 +28,16 @@ public func min<T: Comparable>(_ x: T, _ y: T) -> T {
 ///   - rest: Zero or more additional values.
 /// - Returns: The least of all the arguments. If there are multiple equal
 ///   least arguments, the result is the first one.
+
+
 @inlinable // protocol-only
 public func min<T: Comparable>(_ x: T, _ y: T, _ z: T, _ rest: T...) -> T {
-  var minValue = min(min(x, y), z)
-  // In case `value == minValue`, we pick `minValue`. See min(_:_:).
-  for value in rest where value < minValue {
-    minValue = value
-  }
-  return minValue
+    var minValue = min(min(x, y), z)
+    // In case `value == minValue`, we pick `minValue`. See min(_:_:).
+    for value in rest where value < minValue {
+        minValue = value
+    }
+    return minValue
 }
 
 /// Returns the greater of two comparable values.
@@ -52,8 +48,8 @@ public func min<T: Comparable>(_ x: T, _ y: T, _ z: T, _ rest: T...) -> T {
 /// - Returns: The greater of `x` and `y`. If `x` is equal to `y`, returns `y`.
 @inlinable // protocol-only
 public func max<T: Comparable>(_ x: T, _ y: T) -> T {
-  // In case `x == y`, we pick `y`. See min(_:_:).
-  return y >= x ? y : x
+    // In case `x == y`, we pick `y`. See min(_:_:).
+    return y >= x ? y : x
 }
 
 /// Returns the greatest argument passed.
@@ -67,13 +63,16 @@ public func max<T: Comparable>(_ x: T, _ y: T) -> T {
 ///   greatest arguments, the result is the last one.
 @inlinable // protocol-only
 public func max<T: Comparable>(_ x: T, _ y: T, _ z: T, _ rest: T...) -> T {
-  var maxValue = max(max(x, y), z)
-  // In case `value == maxValue`, we pick `value`. See min(_:_:).
-  for value in rest where value >= maxValue {
-    maxValue = value
-  }
-  return maxValue
+    var maxValue = max(max(x, y), z)
+    // In case `value == maxValue`, we pick `value`. See min(_:_:).
+    for value in rest where value >= maxValue {
+        maxValue = value
+    }
+    return maxValue
 }
+
+
+
 
 /// An enumeration of the elements of a sequence or collection.
 ///
@@ -91,70 +90,78 @@ public func max<T: Comparable>(_ x: T, _ y: T, _ z: T, _ rest: T...) -> T {
 ///     }
 ///     // Prints "0: foo"
 ///     // Prints "1: bar"
+
+
+/*
+    相比, Index 之前是需要人工进行控制, Enumerate 是将 Index 的加减内置到了算法里面.
+ */
 @frozen
 public struct EnumeratedSequence<Base: Sequence> {
-  @usableFromInline
-  internal var _base: Base
-
-  /// Construct from a `Base` sequence.
-  @inlinable
-  internal init(_base: Base) {
-    self._base = _base
-  }
+    @usableFromInline
+    internal var _base: Base
+    
+    /// Construct from a `Base` sequence.
+    @inlinable
+    internal init(_base: Base) {
+        self._base = _base
+    }
 }
 
+/*
+    EnumeratedSequence 的 Iterator,
+ */
 extension EnumeratedSequence {
-  /// The iterator for `EnumeratedSequence`.
-  ///
-  /// An instance of this iterator wraps a base iterator and yields
-  /// successive `Int` values, starting at zero, along with the elements of the
-  /// underlying base iterator. The following example enumerates the elements of
-  /// an array:
-  ///
-  ///     var iterator = ["foo", "bar"].enumerated().makeIterator()
-  ///     iterator.next() // (0, "foo")
-  ///     iterator.next() // (1, "bar")
-  ///     iterator.next() // nil
-  ///
-  /// To create an instance, call
-  /// `enumerated().makeIterator()` on a sequence or collection.
-  @frozen
-  public struct Iterator {
-    @usableFromInline
-    internal var _base: Base.Iterator
-    @usableFromInline
-    internal var _count: Int
-
-    /// Construct from a `Base` iterator.
-    @inlinable
-    internal init(_base: Base.Iterator) {
-      self._base = _base
-      self._count = 0
+    /// The iterator for `EnumeratedSequence`.
+    ///
+    /// An instance of this iterator wraps a base iterator and yields
+    /// successive `Int` values, starting at zero, along with the elements of the
+    /// underlying base iterator. The following example enumerates the elements of
+    /// an array:
+    ///
+    ///     var iterator = ["foo", "bar"].enumerated().makeIterator()
+    ///     iterator.next() // (0, "foo")
+    ///     iterator.next() // (1, "bar")
+    ///     iterator.next() // nil
+    ///
+    /// To create an instance, call
+    /// `enumerated().makeIterator()` on a sequence or collection.
+    @frozen
+    public struct Iterator {
+        @usableFromInline
+        internal var _base: Base.Iterator
+        @usableFromInline
+        internal var _count: Int
+        
+        /// Construct from a `Base` iterator.
+        @inlinable
+        internal init(_base: Base.Iterator) {
+            self._base = _base
+            self._count = 0
+        }
     }
-  }
 }
 
 extension EnumeratedSequence.Iterator: IteratorProtocol, Sequence {
-  /// The type of element returned by `next()`.
-  public typealias Element = (offset: Int, element: Base.Element)
-
-  /// Advances to the next element and returns it, or `nil` if no next element
-  /// exists.
-  ///
-  /// Once `nil` has been returned, all subsequent calls return `nil`.
-  @inlinable
-  public mutating func next() -> Element? {
-    guard let b = _base.next() else { return nil }
-    let result = (offset: _count, element: b)
-    _count += 1 
-    return result
-  }
+    /// The type of element returned by `next()`.
+    public typealias Element = (offset: Int, element: Base.Element)
+    
+    /// Advances to the next element and returns it, or `nil` if no next element
+    /// exists.
+    ///
+    /// Once `nil` has been returned, all subsequent calls return `nil`.
+    @inlinable
+    public mutating func next() -> Element? {
+        guard let b = _base.next() else { return nil }
+        let result = (offset: _count, element: b)
+        _count += 1
+        return result
+    }
 }
 
 extension EnumeratedSequence: Sequence {
-  /// Returns an iterator over the elements of this sequence.
-  @inlinable
-  public __consuming func makeIterator() -> Iterator {
-    return Iterator(_base: _base.makeIterator())
-  }
+    /// Returns an iterator over the elements of this sequence.
+    @inlinable
+    public __consuming func makeIterator() -> Iterator {
+        return Iterator(_base: _base.makeIterator())
+    }
 }
